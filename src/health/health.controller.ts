3489963b1e216c8dc085249e1ba2c898aa9b5ec4
Yuri Novikov <yuri@freakingloud.net>
2025-04-29 17:40:03 +0300

    add open api

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get, HttpCode } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('health')
 export class HealthController {
   @Get()
   @HttpCode(200)
+  @ApiResponse({
+    status: 200,
+    description: 'All is fine',
+  })
   check() {
     return {
       status: 'ok',
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { corsConfig } from './config/cors.config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   if (process.env.CORS_ENABLED === 'true') {
     app.enableCors(corsConfig());
   }
+
+  const config = new DocumentBuilder()
+    .setTitle('Code Metrics Lab API')
+    .setDescription('Backend part of Code Metrics Lab')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 4000);
 }
